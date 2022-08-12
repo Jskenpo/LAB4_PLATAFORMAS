dbd23a38a1abfd98345c96b018a6043db45dccc5
jsantis <san21153@uvg.edu.gt>
2022-08-11 18:02:45 -0600

    avances en main activity

--- LAB4/app/src/main/java/com/plataformas/lab4/MainActivity.kt
+++ LAB4/app/src/main/java/com/plataformas/lab4/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.plataformas.lab4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonNextActivity: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonNextActivity = findViewById(R.id.btn_mainActivity_toNextActivity)
+
+        initListeners()
+    }
+
+    fun initListeners(){
+        buttonNextActivity.setOnClickListener {
+            val intent = Intent(this, AppActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
