3c4f8361ee01f660bc6c54b0df97e567e29e24e1
jsantis <san21153@uvg.edu.gt>
2022-08-11 21:26:28 -0600

    toast message

--- LAB4/app/src/main/java/com/plataformas/lab4/AppActivity.kt
+++ LAB4/app/src/main/java/com/plataformas/lab4/AppActivity.kt
@@ -2,10 +2,19 @@ package com.plataformas.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class AppActivity : AppCompatActivity() {
+
+    lateinit var toastmsg: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+        toastmsg = findViewById(R.id.boton)
+
+        toastmsg.setOnClickListener {
+            Toast.makeText(this, "José Pablo Santisteban Vargas", Toast.LENGTH_LONG).show()
+        }
     }
 }
