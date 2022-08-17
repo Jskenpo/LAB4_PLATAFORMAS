e5591ed9b36211e4579e71bdf8aeb02e78bd4110
jsantis <san21153@uvg.edu.gt>
2022-08-16 23:07:41 -0600

    actualización de parametros

--- LAB4New/app/src/main/java/com/plataformas/lab4new/MainActivity.kt
+++ LAB4New/app/src/main/java/com/plataformas/lab4new/MainActivity.kt
@@ -1,23 +1,52 @@
 package com.plataformas.lab4new
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toastmsg: Button
+    lateinit var descarga: Button
+    lateinit var maps: ImageView
+    lateinit var deti: Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toastmsg = findViewById(R.id.boton)
+        descarga = findViewById(R.id.descarga)
+        maps = findViewById(R.id.imagen)
+        deti = findViewById(R.id.det)
         initlisteners()
     }
     private fun initlisteners(){
         toastmsg.setOnClickListener {
             Toast.makeText(this, "José Pablo Santisteban Vargas", Toast.LENGTH_LONG).show()
         }
+
+        maps.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, android.net.Uri.parse("https://goo.gl/maps/zfwz6MMrsGXVHL887"))
+            startActivity(intent)
+        }
+
+        descarga.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, android.net.Uri.parse("https://play.google.com/store/apps/details?id=com.whatsapp"))
+            startActivity(intent)
+        }
+
+
+
+
+
     }
+
+    
+
+
 }
---
+++ LAB4New/app/src/main/java/com/plataformas/lab4new/PermisosActivity.kt
@@ -0,0 +1,11 @@
+package com.plataformas.lab4new
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PermisosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permisos)
+    }
+}
